ae0c0ede00c1c6aad56f994725dd18818b6a0c4d
munirahenad <muneraenad@hotmail.com>
2025-12-22 08:37:11 +0000

    feat: create main shell header file with core definitions

---
+++ shell.h
@@ -0,0 +1,18 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point for simple shell
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0 on success
+ */
+int main(int ac, char **av);
+
+#endif /* SHELL_H */
+
