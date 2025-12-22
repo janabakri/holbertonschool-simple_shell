12dc7e79a0a4769d642a7bd63c26bbcb661382a5
munirahenad <muneraenad@hotmail.com>
2025-12-22 08:42:29 +0000

    refactor: enhance shell header with state management and external dependencies

--- shell.h
+++ shell.h
@@ -1,18 +1,30 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+extern char **environ;
 
 /**
- * main - Entry point for simple shell
- * @ac: argument count
- * @av: argument vector
- *
- * Return: 0 on success
+ * struct shell_state - Shell runtime state
+ * @argv0: program name (argv[0])
+ * @line_number: current line number (1-based)
  */
-int main(int ac, char **av);
+typedef struct shell_state
+{
+	char *argv0;
+	unsigned long line_number;
+} shell_state_t;
+
+int prompt_if_interactive(void);
+int run_line(shell_state_t *st, char *line);
+int execute_command(shell_state_t *st, char *cmd);
+void print_not_found(shell_state_t *st, const char *cmd);
 
 #endif /* SHELL_H */
 
