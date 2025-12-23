cdef314c63ca609deb0433aa426203151c846fd2
munirahenad <muneraenad@hotmail.com>
2025-12-23 22:06:46 +0000

    Fix function prototypes and header inconsistencies

--- shell.h
+++ shell.h
@@ -3,7 +3,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -11,7 +10,7 @@
 extern int last_status;
 extern char **environ;
 
-/* custom getline (you still need to implement it) */
+/* custom getline */
 ssize_t my_getline(char **lineptr, size_t *n, int fd);
 
 /* builtins */
@@ -26,6 +25,12 @@ char **split_line(char *line);
 void free_argv(char **argv);
 char *find_path(char *command, char **env);
 void print_error(char *prog, char *cmd, int count);
+
+/* custom string helpers (replace forbidden functions) */
+int _strcmp(const char *s1, const char *s2);
+int _strlen(const char *s);
+char *_strdup(const char *s);
+char *_strchr(const char *s, char c);
 
 #endif /* SHELL_H */
 
