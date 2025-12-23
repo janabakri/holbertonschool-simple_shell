6fd3fbf71fa95a2cdeaeecec55571fbef31b3232
munirahenad <muneraenad@hotmail.com>
2025-12-23 21:58:24 +0000

    Main loop now uses custom getline instead of standard library version

--- main.c
+++ main.c
@@ -2,6 +2,74 @@
 
 int last_status = 0;
 
+#define READ_BUF_SIZE 1024
+
+ssize_t my_getline(char **lineptr, size_t *n, int fd)
+{
+	static char buf[READ_BUF_SIZE];
+	static ssize_t buf_pos = 0;
+	static ssize_t buf_len = 0;
+
+	size_t out_len = 0;
+	char *out;
+
+	if (!lineptr || !n || fd < 0)
+		return (-1);
+
+	if (*lineptr == NULL || *n == 0)
+	{
+		*n = 128;
+		*lineptr = malloc(*n);
+		if (!*lineptr)
+			return (-1);
+	}
+
+	out = *lineptr;
+
+	while (1)
+	{
+		if (buf_pos >= buf_len)
+		{
+			buf_len = read(fd, buf, READ_BUF_SIZE);
+			buf_pos = 0;
+
+			if (buf_len <= 0)
+			{
+				if (out_len == 0)
+					return (-1);
+				out[out_len] = '\0';
+				return ((ssize_t)out_len);
+			}
+		}
+
+		while (buf_pos < buf_len)
+		{
+			char c = buf[buf_pos++];
+
+			if (out_len + 2 > *n)
+			{
+				size_t new_n = (*n) * 2;
+				char *tmp = realloc(out, new_n);
+
+				if (!tmp)
+					return (-1);
+
+				out = tmp;
+				*lineptr = out;
+				*n = new_n;
+			}
+
+			out[out_len++] = c;
+
+			if (c == '\n')
+			{
+				out[out_len] = '\0';
+				return ((ssize_t)out_len);
+			}
+		}
+	}
+}
+
 int main(int ac, char **argv, char **env)
 {
 	char *line = NULL;
@@ -16,7 +84,6 @@ int main(int ac, char **argv, char **env)
 		if (isatty(STDIN_FILENO))
 			write(STDOUT_FILENO, "($) ", 4);
 
-		/* Task 0.1.1: use custom getline (NOT getline) */
 		read_bytes = my_getline(&line, &len, STDIN_FILENO);
 		if (read_bytes == -1)
 		{
