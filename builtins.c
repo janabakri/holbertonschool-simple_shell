c52a8f58810ff9c46fd1318af7f395f36a8826b1
munirahenad <muneraenad@hotmail.com>
2025-12-23 21:46:05 +0000

    - Added handle_exit() function to exit shell with last status code

--- builtins.c
+++ builtins.c
@@ -1,20 +1,19 @@
 #include "shell.h"
 
-/* تنفيذ built-in exit باستخدام آخر حالة خروج */
 void handle_exit(void)
 {
-    exit(last_status);
+	exit(last_status);
 }
 
-/* تنفيذ built-in env */
-void handle_env(void)
+void handle_env(char **env)
 {
-    int i;
+	int i = 0;
 
-    for (i = 0; environ[i]; i++)
-    {
-        write(STDOUT_FILENO, environ[i], strlen(environ[i]));
-        write(STDOUT_FILENO, "\n", 1);
-    }
+	while (env && env[i])
+	{
+		write(STDOUT_FILENO, env[i], strlen(env[i]));
+		write(STDOUT_FILENO, "\n", 1);
+		i++;
+	}
 }
 
