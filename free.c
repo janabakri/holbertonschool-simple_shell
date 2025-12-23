9892f1b16ca67356503f6969eda52a322d826e20
munirahenad <muneraenad@hotmail.com>
2025-12-23 22:03:09 +0000

    Add custom string utility functions

--- free.c
+++ free.c
@@ -1,14 +1,61 @@
 #include "shell.h"
 
-void free_argv(char **argv)
+int _strcmp(const char *s1, const char *s2)
 {
-    int i;
+	while (*s1 && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	return ((unsigned char)*s1 - (unsigned char)*s2);
+}
 
-    if (!argv)
-        return;
+int _strlen(const char *s)
+{
+	int n = 0;
 
-    for (i = 0; argv[i]; i++)
-        free(argv[i]);
-    free(argv);
+	if (!s)
+		return (0);
+
+	while (s[n])
+		n++;
+	return (n);
+}
+
+char *_strdup(const char *s)
+{
+	int len, i;
+	char *p;
+
+	if (!s)
+		return (NULL);
+
+	len = _strlen(s);
+	p = malloc(len + 1);
+	if (!p)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		p[i] = s[i];
+	p[i] = '\0';
+
+	return (p);
+}
+
+char *_strchr(const char *s, char c)
+{
+	if (!s)
+		return (NULL);
+
+	while (*s)
+	{
+		if (*s == c)
+			return ((char *)s);
+		s++;
+	}
+	if (c == '\0')
+		return ((char *)s);
+
+	return (NULL);
 }
 
