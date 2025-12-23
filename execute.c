4cfdd6baa154cfc4c7619043236db13f873e1ba8
munirahenad <muneraenad@hotmail.com>
2025-12-23 21:40:15 +0000

    Implement execute_command function for external command execution

--- execute.c
+++ execute.c
@@ -2,42 +2,54 @@
 
 int execute_command(char *line, char *prog, char **env, int count)
 {
-    char **argv;
-    char *path;
-    pid_t pid;
-    int status;
+	char **argv;
+	char *path;
+	pid_t pid;
+	int status;
 
-    argv = split_line(line);
-    if (!argv || !argv[0])
-    {
-        free_argv(argv);
-        return 0;
-    }
+	argv = split_line(line);
+	if (!argv || !argv[0])
+	{
+		free_args(argv);
+		return (0);
+	}
 
-    path = find_path(argv[0], env);
-    if (!path)
-    {
-        print_error(prog, argv[0], count);
-        free_argv(argv);
-        return 127;
-    }
+	path = find_path(argv[0], env);
+	if (!path)
+	{
+		print_error(prog, argv[0], count);
+		free_args(argv);
+		return (127);
+	}
 
-    pid = fork();
-    if (pid == 0)
-    {
-        execve(path, argv, env);
-        print_error(prog, argv[0], count);
-        exit(127);
-    }
-    else
-    {
-        waitpid(pid, &status, 0);
-        free(path);
-        free_argv(argv);
-        if (WIFEXITED(status))
-            return WEXITSTATUS(status);
-    }
+	pid = fork();
+	if (pid == -1)
+	{
+		free(path);
+		free_args(argv);
+		return (1);
+	}
 
-    return 0;
+	if (pid == 0)
+	{
+		execve(path, argv, env);
+		print_error(prog, argv[0], count);
+		_exit(127);
+	}
+
+	if (waitpid(pid, &status, 0) == -1)
+	{
+		free(path);
+		free_args(argv);
+		return (1);
+	}
+
+	free(path);
+	free_args(argv);
+
+	if (WIFEXITED(status))
+		return (WEXITSTATUS(status));
+
+	return (1);
 }
 
